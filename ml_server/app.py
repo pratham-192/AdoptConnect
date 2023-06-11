42b45674da3912d9e062ab9e5543bbfbddde400a
Chaitanya Giri <67907435+Chaitanya134@users.noreply.github.com>
2023-06-12 03:31:35 +0530

    Fix CORS

--- ml_server/app.py
+++ ml_server/app.py
@@ -1,11 +1,11 @@
 import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import openai
 from dotenv import load_dotenv
-
-app = FastAPI()
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 # env variables
 load_dotenv("./.env.local")
@@ -13,13 +13,25 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # CORS
 origins = ["https://adopt-connect.vercel.app", "http://localhost"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
+
+app = FastAPI(middleware=middleware)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 class ConversationHistory(BaseModel):
     history: str
